ca1bf7d9fca9866f4dd4c2fddecddfc023f3750b
Trần Nhật Duy <trannhatduy297@gmail.com>
2017-07-28 03:32:52 +0700

    Add error page

---
+++ src/resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.topnav')
+
+@section('page-header','')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="box-body">
+                    <h3>Lỗi: 403 - Bạn không có quyền thực hiện thao tác này.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ src/resources/views/errors/500.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.topnav')
+
+@section('page-header','')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-primary">
+                <div class="box-body">
+                    <h3>Lỗi: 500 - Lỗi máy chủ.</h3>
+                    <p>Vui lòng liên hệ admin qua email: <a href="mailto:hi@duydev.me">hi@duydev.me</a>. Chân thành cảm ơn.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
